a46f8389cf2185a3024394d0bc84d5aae6cbb1cf
Teerapat Limpawittayakul <tlimpawittayakul@gmail.com>
2023-03-15 20:07:05 +0700

    create Exercise1.js

---
+++ Exercise1.js
@@ -0,0 +1,12 @@
+//part1
+const Name = ['Sofia', 'David', 'Juan'];
+
+//part2
+Name.push('Sara', 'Augustin');
+Name.shift();
+console.log(Name);
+
+//part3
+Name.splice(1,0,'Reneta');
+Name.push('Elena');
+console.log(Name);
